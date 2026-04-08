f547898ef75249093dc2328ad2e0f8e40f6a2660
Astitwa Saxena <astitwa.94@gmail.com>
2026-04-08 11:29:16 +0530

    Enhance MicStage with UI callback for amplitude
    Added ui_callback parameter to MicStage for UI amplitude updates.

--- audio/mic.py
+++ audio/mic.py
@@ -4,10 +4,11 @@ from core.stage import Stage
 
 
 class MicStage(Stage):
-    def __init__(self, output_q, samplerate=16000, blocksize=1024):
+    def __init__(self, output_q, samplerate=16000, blocksize=1024, ui_callback=None):
         super().__init__("MicStage", None, output_q)
         self.samplerate = samplerate
         self.blocksize = blocksize
+        self.ui_callback = ui_callback  # NEW
 
     def run(self):
         def callback(indata, frames, time, status):
@@ -17,7 +18,18 @@ class MicStage(Stage):
             # Convert safely to numpy array
             audio_chunk = np.array(indata, copy=True)
 
+            # Send to pipeline
             self.output_q.put(audio_chunk)
+
+            #send amplitude to UI
+            if self.ui_callback:
+                # normalize int16 → float
+                audio_float = audio_chunk.astype(np.float32) / 32768.0
+
+                # RMS amplitude
+                amplitude = float(np.sqrt(np.mean(audio_float**2)))
+
+                self.ui_callback(amplitude)
 
         with sd.InputStream(
             samplerate=self.samplerate,
