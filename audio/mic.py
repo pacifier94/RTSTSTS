9ea25f7fa7c755c933a62098042ea7c534c5f18a
Astitwa Saxena <astitwa.94@gmail.com>
2026-04-13 12:20:14 +0530

    Refactor audio processing to simplify amplitude handling
    Removed threshold checks for audio amplitude before sending to output queue.

--- audio/mic.py
+++ audio/mic.py
@@ -8,8 +8,7 @@ class MicStage(Stage):
         super().__init__("MicStage", None, output_q)
         self.samplerate = samplerate
         self.blocksize = blocksize
-        self.ui_callback = ui_callback 
-        self.threshold = threshold 
+        self.ui_callback = ui_callback  # NEW
 
     def run(self):
         def callback(indata, frames, time, status):
@@ -19,13 +18,9 @@ class MicStage(Stage):
             # Convert safely to numpy array
             audio_chunk = np.array(indata, copy=True)
 
-           # compute amplitude
-            audio_float = audio_chunk.astype(np.float32) / 32768.0
-            amplitude = float(np.sqrt(np.mean(audio_float**2)))
+            # Send to pipeline
+            self.output_q.put(audio_chunk)
 
-            # only send if above threshold
-            if amplitude > self.threshold:
-                self.output_q.put(audio_chunk)
             #send amplitude to UI
             if self.ui_callback:
                 # normalize int16 → float
@@ -35,8 +30,6 @@ class MicStage(Stage):
                 amplitude = float(np.sqrt(np.mean(audio_float**2)))
 
                 self.ui_callback(amplitude)
-                if amplitude > self.threshold:
-                    self.output_q.put(audio_chunk)
 
         with sd.InputStream(
             samplerate=self.samplerate,
