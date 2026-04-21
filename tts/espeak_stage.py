b3c3325b51d012c6bbae94ef3e20cf0f4950497b
Astitwa Saxena <astitwa.94@gmail.com>
2026-04-21 19:19:25 +0530

    Update EspeakStage to use espeak and process data
    Refactor process method to accept data dictionary and use espeak instead of espeak-ng.

--- tts/espeak_stage.py
+++ tts/espeak_stage.py
@@ -1,26 +1,30 @@
 import subprocess
+import shutil
 from core.stage import Stage
-
-
+from indic_transliteration import sanscript
+from shared_state import is_speaking
 class EspeakStage(Stage):
     def __init__(self, input_q):
         super().__init__("TTS", input_q, None)
+        # Check path for espeak-ng
+        self.executable = shutil.which("espeak-ng")
 
-    def process(self, text):
-        if text is None:
+    def process(self, data):
+        if not data:
             return None
 
-        text = text.strip()
-        if text == "":
-            return None
+        text = data.get("text", "")
 
         try:
+            is_speaking.set()   #START SPEAKING
+
             subprocess.run(
-                ["espeak-ng", "-v", "bn", text],
+                ["espeak", text],
                 stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
             )
-        except Exception as e:
-            print("TTS error:", e)
+
+        finally:
+            is_speaking.clear()  #DONE SPEAKING
 
         return None
