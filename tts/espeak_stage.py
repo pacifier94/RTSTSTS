d0e5caf67cdc01898469eb2fe8731c3111f31be7
Astitwa Saxena <astitwa.94@gmail.com>
2026-04-22 17:00:10 +0530

    Refactor EspeakStage to use asynchronous speaking
    Refactor speaking functionality to run asynchronously using threading. Removed synchronous subprocess execution and added a new method for asynchronous speech.

--- tts/espeak_stage.py
+++ tts/espeak_stage.py
@@ -1,30 +1,34 @@
 import subprocess
-import shutil
+import threading
 from core.stage import Stage
-from indic_transliteration import sanscript
 from shared_state import is_speaking
+
 class EspeakStage(Stage):
     def __init__(self, input_q):
         super().__init__("TTS", input_q, None)
-        # Check path for espeak-ng
-        self.executable = shutil.which("espeak-ng")
+
+    def speak_async(self, text):
+        def run():
+            try:
+                is_speaking.set()
+
+                subprocess.run(
+                    ["espeak", text],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+
+            finally:
+                is_speaking.clear()
+
+        threading.Thread(target=run, daemon=True).start()
 
     def process(self, data):
         if not data:
             return None
 
         text = data.get("text", "")
-
-        try:
-            is_speaking.set()   #START SPEAKING
-
-            subprocess.run(
-                ["espeak", text],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL
-            )
-
-        finally:
-            is_speaking.clear()  #DONE SPEAKING
+        if text:
+            self.speak_async(text)
 
         return None
